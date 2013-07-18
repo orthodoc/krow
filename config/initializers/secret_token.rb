e4e153c1d0135e555f6b91e47fffdc2a96a4c63a
Biswajit Dutta Baruah <bdbaruah@orthodoc.in>
2013-07-18 17:49:04 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Krow::Application.config.secret_key_base = 'ee1d5fa1e16effa751229de0aac33890b9992ec22ce327c5789d403e07302ccc01aed67ea0ae99509f90b02f636473973e3b3dba3167a6e4bd6454f8c7e14a7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Krow::Application.config.session_store :cookie_store, key: '_krow_session'
